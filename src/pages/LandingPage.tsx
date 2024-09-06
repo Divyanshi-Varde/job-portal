03e489aa8cd8742358a6413fdb40f1d439d3703b
Divyanshi Varde <divyanshi.varde@propelius.tech>
2024-09-06 12:19:37 +0530

    Landing page completed

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,33 +1,114 @@
-// import { Accordion } from "@/components/ui/accordion"
-
+import { Button } from "@/components/ui/button";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+} from "@/components/ui/carousel";
+import { Link } from "react-router-dom";
+import companies from "@/data/companies.json";
+import faqs from "@/data/faq.json";
+import Autoplay from "embla-carousel-autoplay";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
 
 const LandingPage = () => {
+  const autoplayOptions: any = Autoplay({ delay: 2000 });
   return (
     <main className="flex flex-col gap-10 sm:gap-20 py-10 sm:py-20">
       <section className="text-center">
-        <h1 className="flex flex-col items-center justify-center 
-        gradient-title text-4xl font-extrabold sm:text-6xl lg:text-8xl tracking-tighter py-4">
-          Find your Dream Job 
+        <h1
+          className="flex flex-col items-center justify-center 
+        gradient-title text-4xl font-extrabold sm:text-6xl lg:text-8xl tracking-tighter py-4"
+        >
+          Find your Dream Job
           <span className="flex items-center gap-2 sm:gap-6">
-            and get <img src="/logo.png" alt="Hirrd Logo" className="h-14 sm:h-24 lg:h-32"/>
-          </span></h1>
-          <p className="text-gray-300 sm:mt-4 text-xs lg:text-xl">
-            Explore thousands of job listings or find the perfect candidate
-          </p>
+            and get{" "}
+            <img
+              src="/logo.png"
+              alt="Hirrd Logo"
+              className="h-14 sm:h-24 lg:h-32"
+            />
+          </span>
+        </h1>
+        <p className="text-gray-300 sm:mt-4 text-xs lg:text-xl">
+          Explore thousands of job listings or find the perfect candidate
+        </p>
       </section>
-      <div>
+
       {/* {buttons} */}
+      <div className="flex gap-6 justify-center">
+        <Link to="/jobs">
+          <Button variant={"blue"} size={"xl"}>
+            Find Jobs
+          </Button>
+        </Link>
+
+        <Link to="/post-jobs">
+          <Button variant={"destructive"} size={"xl"}>
+            Post a Job
+          </Button>
+        </Link>
       </div>
+
       {/* {carousel} */}
+      <Carousel className="w-full py-10" plugins={[autoplayOptions]}>
+        <CarouselContent className="flex gap-5 sm:gap-20 items-center">
+          {companies.map(({ name, id, path }) => {
+            return (
+              <CarouselItem key={id} className="basis-1/3 lg:basis-1/6">
+                <img
+                  src={path}
+                  alt={name}
+                  className="h-9 sm:h-14 object-contain"
+                />
+              </CarouselItem>
+            );
+          })}
+        </CarouselContent>
+      </Carousel>
 
       {/* {banner} */}
-      <section>
+      <img src="banner.jpeg" alt="banner" className="w-full" />
+
       {/* {cards} */}
+      <section className="grid grid-cols-1 md:grid-cols-2 gap-4">
+        <Card className="bg-gradient-to-r from-blue-200 to-blue-600 text-black">
+          <CardHeader>
+            <CardTitle>For Job Seekers</CardTitle>
+          </CardHeader>
+          <CardContent>
+            Search and apply for jobs, track applications and more.
+          </CardContent>
+        </Card>
+
+        <Card className="bg-gradient-to-r from-blue-200 to-blue-600 text-black">
+          <CardHeader>
+            <CardTitle>For Employers</CardTitle>
+          </CardHeader>
+          <CardContent>
+            <p>Post jobs, manage applications and find the best candidates. </p>
+          </CardContent>
+        </Card>
       </section>
 
       {/* {Accordion} */}
+      <Accordion type="single" collapsible>
+        {faqs.map((faq, index) => {
+          return (
+            <AccordionItem key={index} value={`item-${index + 1}`}>
+              <AccordionTrigger>{faq.question}</AccordionTrigger>
+              <AccordionContent>{faq.answer}</AccordionContent>
+            </AccordionItem>
+          );
+        })}
+      </Accordion>
     </main>
-  )
-}
+  );
+};
 
-export default LandingPage
+export default LandingPage;
